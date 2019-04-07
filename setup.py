52227708a6fafee7c775ddfbb59c2163ba238267
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-04-07 15:20:08 +0530

    modified for final

--- setup.py
+++ setup.py
@@ -1,23 +1,46 @@
-import setuptools
+from setuptools import setup
+import os
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="wisp",
-    version="0.0.8-b",
-    author="Jimut Bahan Pal",
-    author_email="jimutbahanpal@yahoo.com",
-    description="A preference based location finder application",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Jimut123/wisp",
-    install_requires=['requests', 'pandas','folium','geopy','numpy','wget','datetime','IPython','pip'],
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+if __name__ == '__main__':
+    setup(
+        name = 'wisp',
+        version="0.0.8-beta",
+        description = 'A preference based location finder app',
+        author = 'Jimut Bahan Pal',
+        author_email = 'paljimutbahan@gmail.com',
+        maintainer = 'Jimut Bahan Pal',
+        maintainer_email = 'paljimutbahan@gmail.com',
+        url = '',
+        license = 'GPLv2+',
+        platforms = 'Linux',
+        py_modules = ['wisp'],
+        entry_points = {
+            'console_scripts': ['wisp = wisp:main'],
+        },
+        include_package_data = True,
+        install_requires = [
+            'requests',
+            'datetime',
+            'IPython',
+            'pandas',
+            'folium',
+            'geopy',     
+            'numpy',
+            'wget',
+            'pip'
+        ],
+        keywords = 'Preference, location, wisp, requests, html, json, numpy, pandas, software, tkinter, folium, geopy, lat lon, wget, Ipython, location cluster, KNN, Machine learning, clustering',
+        classifiers = [
+                'Development Status :: 0.0.8 - Beta',
+                'Environment :: Console',
+                'Intended Audience :: End Users/Desktop',
+                'Intended Audience :: System Administrators',
+                'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+                'Operating System :: Manjaro',
+                'Programming Language :: Python :: 3.7.2',
+                'Topic :: Internet :: WWW/HTTP',
+                'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+                ],
 )
-
